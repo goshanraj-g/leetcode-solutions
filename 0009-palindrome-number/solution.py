200cdca8a0bafc947b005dd4b6c320786a44a52f
Goshanraj Govindaraj <govindag@mcmaster.ca>
2024-10-18 06:54:56 +0000

    [LeetCode Sync] Runtime - 24 ms (12.69%), Memory - 11.8 MB (100.00%)

---
+++ 0009-palindrome-number/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x_str = str(x)
+        if x_str == x_str[::-1]:
+            return True
+        else:
+            return False
+
+        
