3588861a42d00b84ce30626bcf3ae41d2a70e5dd
Goshanraj Govindaraj <govindag@mcmaster.ca>
2024-12-17 20:31:36 +0000

    [LeetCode] Runtime - 4023 ms (5.48%), Memory - 13.2 MB (35.37%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,21 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        solution = []
-        elementCounter = 0
-        numCounter = 0
-        for element in nums:
-            for num in nums:
-                if element + num == target and not (elementCounter == numCounter):
-                    solution.append(elementCounter)
-                    solution.append(numCounter)
-                    return solution
-                numCounter += 1
-            elementCounter += 1
-            numCounter = 0
-
+        for i in range(len(nums)-1):
+            for j in range(1, len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i, j]
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        """ 
+        """
         
