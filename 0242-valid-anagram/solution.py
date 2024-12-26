c8769cc7267f8e09adc2062367ebcfc725132484
Goshanraj Govindaraj <govindag@mcmaster.ca>
2024-12-26 21:38:39 +0000

    [LeetCode] Runtime - 17 ms (71.33%), Memory - 14.4 MB (9.21%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,21 +1,16 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        list1 = []
-        list2 = []
-        for char in s:
-            list1.append(char)
-        for char in t:
-            list2.append(char)
-        list1.sort()
-        list2.sort()
-        if list1 == list2:
-            return True
-        else:
+        if len(s) != len(t):
             return False
- 
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+
+        return True
+
         
