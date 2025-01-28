632fd1b254de1225fcf0972e805f46622bdb1004
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-01-28 16:00:04 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 13.4 MB (15.83%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)-1):
-            for j in range(1, len(nums)):
-                if nums[i] + nums[j] == target and i != j:
-                    return [i, j]
+
+        stored_val = {}
+
+        for i in range(len(nums)):
+            difference = target - nums[i]
+
+            if difference in stored_val:
+                return [stored_val[difference], i]
+
+            stored_val[nums[i]] = i
         """
         :type nums: List[int]
         :type target: int
