0ee59347685f20840c72db7fdb773481e4befe71
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-12 18:48:19 +0000

    [LeetCode] Runtime - 15 ms (27.52%), Memory - 15.9 MB (19.28%)

--- 0155-min-stack/solution.py
+++ 0155-min-stack/solution.py
@@ -1,27 +1,43 @@
-class MinStack:
+class MinStack(object):
 
     def __init__(self):
         self.stack = []
         self.min_stack = []
         
 
-    def push(self, val: int) -> None:
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
         self.stack.append(val)
         if not self.min_stack or val <= self.min_stack[-1]:
             self.min_stack.append(val)
+        
 
-    def pop(self) -> None:
+    def pop(self):
+        """
+        :rtype: None
+        """
         if self.stack:
             popped = self.stack.pop()
             if popped == self.min_stack[-1]:
                 self.min_stack.pop()
+        
 
-    def top(self) -> int:
+    def top(self):
+        """
+        :rtype: int
+        """
         if self.stack:
             return self.stack[-1]
-        return -1        
+        return -1  
+        
 
-    def getMin(self) -> int:
+    def getMin(self):
+        """
+        :rtype: int
+        """
         if self.min_stack:
             return self.min_stack[-1]
         return -1
