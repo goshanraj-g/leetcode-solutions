ca05f4bb742dc521a3dbfe4660cfcde4f226abbf
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-19 05:46:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.7 MB (40.58%)

---
+++ 2350-find-closest-number-to-zero/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findClosestNumber(self, nums):
+        closest = nums[0]
+        for i in nums:
+            if abs(i) < abs(closest):
+                closest = i
+
+        if closest < 0 and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
