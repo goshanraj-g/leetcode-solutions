4fc1574f2342b5789e03a5f56b1157674d9f2370
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-20 18:37:04 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 12.6 MB (34.22%)

--- 0014-longest-common-prefix/solution.py
+++ 0014-longest-common-prefix/solution.py
@@ -1,19 +1,18 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        #empty case, if it's empty return nothing
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = ""
         if not strs:
-            return ""
-
-        #use first word as the one to compare everything else with
-        prefix = strs[0]
-
-        for string in strs[1:]:
-            while string[:len(prefix)] != prefix:
-                prefix = prefix [:-1]
-                if not prefix:
-                    return ""
+            return prefix
+        
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != char:
+                    return prefix
+            prefix += char
         return prefix
-            
-                
-            
-
+        
