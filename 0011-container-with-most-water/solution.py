c418abb70962062ab767deaac29c476c2b2988a5
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-04-30 20:45:29 +0000

    [LeetCode] Runtime - 117 ms (63.07%), Memory - 20.7 MB (24.99%)

--- 0011-container-with-most-water/solution.py
+++ 0011-container-with-most-water/solution.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def maxArea(self, height):
-        l, r = 0, len(height) - 1
-        tallest = 0
-        while (l < r):
-            h = min(height[l], height[r])
-            window_size = r - l
-            area = h * window_size
-            tallest = max(area, tallest)
-            if height[l] < height[r]:
-                l += 1
-            else:
-                r -= 1
-        return tallest
+        res = 0
+        l, r = 0, len(height)-1
 
+        while (l < r):
+            area = (r - l) * min(height[l], height[r])
+            res = max(area, res)
+            if height[l] < height[r]:
+                l +=1
+            else:
+                r -=1
+        return res
