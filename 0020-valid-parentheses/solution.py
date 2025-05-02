8336b95f0a1b9f25fa23870839056aa164a71001
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-05-02 04:23:33 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 12.4 MB (95.92%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,16 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
         stack = []
-        mapping = {")": "(", "]": "[", "}": "{"}
-        for char in s:
-            if char in "([{":
-                stack.append(char)
-            elif char in ")]}":
-                if not stack or stack[-1] != mapping[char]:
+        closeToOpen = {")" : "(", "}": "{", "]" : "["}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
                     return False
-                stack.pop()
-        return len(stack) == 0
+            else:
+                stack.append(c)
+        if not stack:
+            return True
+        return False
+        
