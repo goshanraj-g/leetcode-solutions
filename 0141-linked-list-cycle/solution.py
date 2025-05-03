12a78c163b3b917653583b505465b617342f7628
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-05-03 02:29:59 +0000

    [LeetCode] Runtime - 34 ms (74.18%), Memory - 19.4 MB (63.74%)

--- 0141-linked-list-cycle/solution.py
+++ 0141-linked-list-cycle/solution.py
@@ -8,9 +8,10 @@ class Solution(object):
     def hasCycle(self, head):
         slow = head
         fast = head
-        while (fast and fast.next):
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
                 return True
         return False
+        
