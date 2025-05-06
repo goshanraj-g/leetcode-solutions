4dce89538f83e71d2e8712b716c7c93f59e86837
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-05-06 23:35:35 +0000

    [LeetCode] Runtime - 157 ms (88.99%), Memory - 19.2 MB (100.00%)

--- 0907-koko-eating-bananas/solution.py
+++ 0907-koko-eating-bananas/solution.py
@@ -1,22 +1,20 @@
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l = 1
-        r = max(piles)
+import math
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        l, r = 1, max(piles)
         res = r
 
         while l <= r:
-            k = (l + r) // 2
+            k = (l + r ) // 2
             hours = 0
             for p in piles:
                 hours += math.ceil(p/k)
-            if hours > h:
-                l = k + 1
-            else:
-                res = min(res, k)
+            
+            if hours <= h:
+                res = min (res, k)
                 r = k - 1
+            else:
+                l = k + 1
         return res
-
-
-
-
+        
         
