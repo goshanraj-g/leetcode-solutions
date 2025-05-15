2f813b98506df700e0b98f412400b5ebebdc0bce
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-05-15 15:03:09 +0000

    [LeetCode] Runtime - 19 ms (22.44%), Memory - 22.7 MB (39.98%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,9 +1,14 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        res = defaultdict(list)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
         for s in strs:
-            count= [0]*26
+            count = [0] * 26
             for c in s:
-                count[ord(c)-ord("a")]+=1
-            res[tuple(count)].append(s)
-        return res.values()
+                count[ord(c) - ord('a')] += 1
+            key = tuple(count)
+            if key in res:
+                res[key].append(s)
+            else:
+                res[key] = [s]
+        return list(res.values())
+
