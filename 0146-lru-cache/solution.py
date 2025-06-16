5975c1d606e9050910c88b7ebbeda7e869c666e9
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-06-16 18:57:26 +0000

    [LeetCode] Runtime - 147 ms (25.02%), Memory - 78.6 MB (29.60%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -2,10 +2,9 @@ class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
         self.prev = self.next = None
+class LRUCache:
 
-
-class LRUCache(object):
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
 
@@ -16,26 +15,30 @@ class LRUCache(object):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
 
+
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev 
+        node.next, node.prev = nxt, prev
 
-
-    def get(self, key):
+    def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
         
-    def put(self, key, value):
+
+    def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
         if len(self.cache) > self.cap:
+            # remove and delete the least recently used
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+        
+
