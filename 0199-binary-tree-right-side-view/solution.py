9cbd124b328db5ff4977c037f94ab3c53937b9b2
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-06-22 21:24:15 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- 0199-binary-tree-right-side-view/solution.py
+++ 0199-binary-tree-right-side-view/solution.py
@@ -8,18 +8,19 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        q = collections.deque()
-        q.append(root)
+        q = collections.deque([root])
 
         while q:
+            rightSide = None
             qLen = len(q)
-            level = []
+            
             for i in range(qLen):
                 node = q.popleft()
                 if node:
-                    level.append(node.val)
+                    rightSide = node
                     q.append(node.left)
                     q.append(node.right)
-            if level:
-                res.append(level[-1])
+            if rightSide:
+                res.append(rightSide.val)
         return res
+        
