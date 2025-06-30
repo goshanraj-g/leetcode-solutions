bbec98f18695a6acd0ddc143ce9d53d2ed392544
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-06-30 15:18:51 +0000

    [LeetCode Sync] Runtime - 20 ms (28.39%), Memory - 23 MB (42.11%)

---
+++ 0124-binary-tree-maximum-path-sum/solution.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+# max path sum w/o split
+        def dfs(root):
+            if not root:
+                return 0
+
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            # max path sum w split
+
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            return root.val + max(leftMax, rightMax)
+        dfs(root)
+        return res[0]
+            
+        
