d40b751407b104083cbd3504ed075a6a54930f3a
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-07-30 16:48:21 +0000

    [LeetCode] Runtime - 139 ms (18.23%), Memory - 32.3 MB (6.04%)

--- 1544-count-good-nodes-in-binary-tree/solution.py
+++ 1544-count-good-nodes-in-binary-tree/solution.py
@@ -7,15 +7,18 @@
 
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, maxVal):
-            if not node:
-                return 0
-            res = 1 if node.val >= maxVal else 0
-            maxVal = max(maxVal, node.val)
-            res += dfs(node.left, maxVal)
-            res += dfs(node.right, maxVal)
-            return res
 
+        # going to be  DFS, where you go down each path, and determine if it's a good node, and add 1
+        def dfs(root, cur_max):
+            if root is None:
+                return 0
+            if root.val >= cur_max:
+                count = 1
+            else:
+                count = 0
+            cur_max = max(cur_max, root.val)
+            right = dfs(root.right, cur_max)
+            left = dfs(root.left, cur_max)
+            return count + right + left
         return dfs(root, root.val)
 
-        
