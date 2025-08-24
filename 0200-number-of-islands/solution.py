4ad31300fbaf7d6419447ce3f3c813256fbe8786
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-08-24 20:47:30 +0000

    [LeetCode] Runtime - 1554 ms (5.01%), Memory - 31.4 MB (15.20%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -1,43 +1,29 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        # iterate through each cell in the grid
-        # if you found an island, increment by 1
-        # check the neighbors (up, down, left, right)
-        # keep track of marked, and unmarked nodes in a set w/ coordinates
-        # when no longer 1's end and continue to next node which is unmarked
+    def numIslands(self, grid):
         if not grid:
             return 0
-    
+        
         rows, cols = len(grid), len(grid[0])
-        visited = set()
-        count = 0
+        visit = set()
+        islands = 0
 
         def bfs(r, c):
             q = collections.deque()
-            visited.add((r, c))
+            visit.add((r, c))
             q.append((r, c))
 
             while q:
-                row, col = q.popleft()
+                r, c = q.popleft() # popping right would mean that this would be an iterative DFS solution
                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
                 for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if (r in range(rows)) and ((c in range(cols)) and (grid[r][c] == '1') and ((r, c) not in visited)):
-                        q.append((r, c))
-                        visited.add((r , c))
-
+                    if (r + dr) in range(rows) and (c + dc) in range(cols) and grid[r+dr][c+dc] == '1' and ((r+dr, c + dc) not in visit):
+                        q.append((r + dr, c + dc))
+                        visit.add((r + dr, c + dc))
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
+                if grid[r][c] == '1' and (r, c) not in visit:
                     bfs(r, c)
-                    count += 1
-
-        return count
-
-            
-
-
-
-
+                    islands += 1
+        return islands
