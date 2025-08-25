b1e39e152bc78b4147e054a6c990aad64f56b232
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-08-25 18:45:05 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 12.6 MB (32.60%)

--- 0733-flood-fill/solution.py
+++ 0733-flood-fill/solution.py
@@ -2,22 +2,28 @@ from collections import deque
 
 class Solution(object):
     def floodFill(self, image, sr, sc, color):
-        original_image = image[sr][sc]
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
         if not image:
             return image
-        elif original_image == color:
+        rows, cols = len(image), len(image[0])
+        orig = image[sr][sc]
+        if orig == color:
             return image
-        rows = len(image)
-        cols = len(image[0])
         q = deque([(sr, sc)])
+        image[sr][sc] = color
         while q:
-            vals = q.popleft()
-            r, c = vals[0], vals[1]
-            if ((r >= 0) and (r < rows)) and ((c >= 0) and (c < cols)) and image[r][c] == original_image:
-                image[r][c] = color
-                directions = [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
-                for direction in directions:
-                    r, c = direction[0], direction[1]
-                    q.append((r, c))
+            r, c = q.popleft()
+            for dr, dc in ((1,0), (0,1), (-1,0), (0,-1)):
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and image[nr][nc] == orig:
+                    image[nr][nc] = color
+                    q.append((nr, nc))
         return image
+
 
