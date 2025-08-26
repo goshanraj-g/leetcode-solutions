bcf81bbac34a808331684205513617418aefd8cb
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-08-26 19:02:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (96.72%)

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -1,19 +1,19 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
 
         res = []
         subset = []
-
         def dfs(i):
             if i >= len(nums):
-                res.append(subset.copy())
+                res.append(subset[:])
                 return
             subset.append(nums[i])
-            
             dfs(i+1)
-
             subset.pop()
-
             dfs(i+1)
         dfs(0)
         return res
