7cc4cfb32a613a5cf2331d43db91b48b88244092
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-08-29 21:59:44 +0000

    [LeetCode] Runtime - 28 ms (11.92%), Memory - 12.7 MB (20.91%)

--- 0039-combination-sum/solution.py
+++ 0039-combination-sum/solution.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def combinationSum(self, candidates, target):
+    def combinationSum(self, candidates, target):  
         res = []
         def dfs(i, total, path):
             if total == target:
@@ -9,16 +9,11 @@ class Solution(object):
                 return
             
             path.append(candidates[i])
-            dfs(i, total + candidates[i], path)
+            dfs(i, total + candidates[i], path) 
             path.pop()
 
-            dfs(i + 1, total, path)
+            dfs(i+1, total, path)
         dfs(0, 0, [])
         return res
-
-
-    # could be similar to subsets, but only add if sum if equal to target, and make it a set to avoid duplicates
-
-
 
 
