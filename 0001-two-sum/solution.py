d294ecabe71a55e8c14289352ac15509e5c9489f
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-09-01 13:56:50 +0000

    [LeetCode] Runtime - 4 ms (47.20%), Memory - 13.1 MB (52.69%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,10 +1,13 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        stored_val = {}
-        for i in range(len(nums)):
-            difference = target - nums[i]
-            if difference in stored_val:
-                return [stored_val[difference], i]
-            stored_val[nums[i]] = i
+class Solution(object):
+    def twoSum(self, nums, target):
 
+        # the approach for this question:
+        # complement
+        stored_val = {}
+
+        for num in range(len(nums)):
+            difference = target - nums[num]
+            if difference in stored_val:
+                return [stored_val[difference], num]
+            stored_val[nums[num]] = num
 
