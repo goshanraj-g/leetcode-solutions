b137e1726e6977468776548653de29f653fca5bd
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-09-03 21:13:16 +0000

    [LeetCode] Runtime - 11 ms (76.34%), Memory - 17.7 MB (100.00%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,21 +1,11 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        if len(s) != len(t):
-            return False
-        dict1, dict2 = {}, {}
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map1 = {}
+        map2 = {}
         for char in s:
-            if char in dict1:
-                dict1[char] += 1
-            else:
-                dict1[char] = 1
+            map1[char] = map1.get(char, 0) + 1
         for char in t:
-            if char in dict2:
-                dict2[char] +=1
-            else:
-                dict2[char] = 1
-        return dict1 == dict2
-
-        return True
+            map2[char] = map2.get(char, 0) + 1
+        return map1 == map2
 
         
