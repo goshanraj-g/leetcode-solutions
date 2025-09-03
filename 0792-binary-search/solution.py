90ca263db97e9a29412326190896ce07422a7a4c
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-09-03 21:14:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.9 MB (6.02%)

--- 0792-binary-search/solution.py
+++ 0792-binary-search/solution.py
@@ -1,22 +1,13 @@
-class Solution(object):
-    def search(self, nums, target):
-
-        left = 0
-        right = len(nums) -1
-
-        while (left <= right):
-            mid = left + (right-left) / 2
-            if (nums[mid] == target):
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
                 return mid
-            elif (nums[mid] < target):
-                left = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
             else:
-                right = mid - 1
+                l = mid + 1
         return -1
-
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
         
