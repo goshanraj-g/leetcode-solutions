2397d15c3f3ec13b8c7d80dff542921942e57444
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-12-23 04:02:10 +0000

    [LeetCode] Runtime - 321 ms (5.07%), Memory - 25 MB (29.17%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -1,26 +1,43 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        NRows = len(grid)
-        NCols = len(grid[0])
-        queue = deque()
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        num_of_islands = 0
+        # iterate through each cell in the grid
+        # if you found an island, increment by 1
+        # check the neighbors (up, down, left, right)
+        # keep track of marked, and unmarked nodes in a set w/ coordinates
+        # when no longer 1's end and continue to next node which is unmarked
+        if not grid:
+            return 0
+    
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        count = 0
 
-        for i in range(NRows):
-            for j in range(NCols):
+        def bfs(r, c):
+            q = collections.deque()
+            visited.add((r, c))
+            q.append((r, c))
 
-                if grid[i][j] == '1':
-                    grid[i][j] = '0'
-                    queue.append((i, j))
-                    num_of_islands += 1
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-                    while queue:
-                        r, c = queue.popleft()
-                        for direction in directions:
-                            nr, nc = r + direction[0], c + direction[1]
-                            if 0 <= nr < NRows and 0 <= nc < NCols and grid[nr][nc] == '1':
-                                queue.append((nr, nc))
-                                grid[nr][nc] = '0'
-        
-        return num_of_islands
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows)) and ((c in range(cols)) and (grid[r][c] == '1') and ((r, c) not in visited)):
+                        q.append((r, c))
+                        visited.add((r , c))
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r, c) not in visited:
+                    bfs(r, c)
+                    count += 1
+
+        return count
+
+            
+
+
+
+
