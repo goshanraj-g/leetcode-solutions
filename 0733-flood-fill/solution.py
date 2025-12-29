8f7940fa372f101d4a1ff7790e039470868c4af3
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-12-29 23:02:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (94.84%)

--- 0733-flood-fill/solution.py
+++ 0733-flood-fill/solution.py
@@ -1,21 +1,23 @@
-class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        rows, cols = len(image), len(image[0])
-        original = image[sr][sc]
-        
-        if color == original:
+from collections import deque
+
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        original_image = image[sr][sc]
+        if not image:
             return image
-        
-        def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols:
-                return
-            if image[r][c] != original:
-                return
-            image[r][c] = color
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-        dfs(sr, sc)
+        elif original_image == color:
+            return image
+        rows = len(image)
+        cols = len(image[0])
+        q = deque([(sr, sc)])
+        while q:
+            vals = q.popleft()
+            r, c = vals[0], vals[1]
+            if ((r >= 0) and (r < rows)) and ((c >= 0) and (c < cols)) and image[r][c] == original_image:
+                image[r][c] = color
+                directions = [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
+                for direction in directions:
+                    r, c = direction[0], direction[1]
+                    q.append((r, c))
         return image
-        
+
