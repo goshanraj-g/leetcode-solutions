77fb80e869d29f4295627d8bb4829b77274819ae
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-12-29 19:20:31 +0000

    [LeetCode Sync] Runtime - 15 ms (68.39%), Memory - 14.7 MB (11.19%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,18 +1,7 @@
 class Solution(object):
     def isPalindrome(self, s):
-        l, r = 0, len(s) - 1
-
-        while l < r:
-            while l < r and not self.alphaNum(s[l]):
-                l += 1
-            while r > l and not self.alphaNum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r- 1
-        return True
-            
-    def alphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
+        res = []
+        for char in s:
+            if char.isalnum():
+                res.append(char.lower())
+        return res == res[::-1]
