2c663cffb9bdc6e3dfcec91f398213868a794747
Goshanraj Govindaraj <govindag@mcmaster.ca>
2026-01-01 22:03:03 +0000

    [LeetCode Sync] Runtime - 37 ms (45.90%), Memory - 12.7 MB (79.55%)

--- 0383-ransom-note/solution.py
+++ 0383-ransom-note/solution.py
@@ -1,6 +1,21 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in set(ransomNote):
-            if magazine.count(i) < ransomNote.count(i):
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        if len(ransomNote) > len(magazine):
+            return False
+        mmap = {}
+        for char in magazine:
+            mmap[char] = mmap.get(char, 0) + 1
+
+        for char in ransomNote:
+            if mmap.get(char, 0) > 0:
+                mmap[char] = mmap.get(char, 0) - 1
+            else:
                 return False
         return True
+
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        
