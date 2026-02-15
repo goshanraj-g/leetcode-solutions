51b6d7f503145cc42be8cf447d10039a40685fcc
Goshanraj Govindaraj <govindag@mcmaster.ca>
2026-02-15 06:23:17 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 12.7 MB (7.75%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,14 +1,15 @@
 class Solution(object):
     def isValid(self, s):
-        mappings = {")":"(", "}":"{", "]":"["}
         stack = []
+        mapping = {")":"(", "}":"{","]":"["}
         for char in s:
-            if char in mappings:
-                if stack and stack[-1] == mappings[char]:
-                    stack.pop()
+            if char in mapping:
+                if len(stack) > 0:
+                    val = stack.pop()
                 else:
+                    return False
+                if mapping[char] != val:
                     return False
             else:
                 stack.append(char)
-        return True if len(stack) == 0 else False
-        
+        return len(stack) == 0
