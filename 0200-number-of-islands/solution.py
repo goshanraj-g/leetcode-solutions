6f00f25cda73b217dbedfc82b0992d8446eff534
Goshanraj Govindaraj <govindag@mcmaster.ca>
2026-04-28 21:35:59 +0000

    [LeetCode] Runtime - 259 ms (30.63%), Memory - 26.8 MB (17.19%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -1,43 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # iterate through each cell in the grid
-        # if you found an island, increment by 1
-        # check the neighbors (up, down, left, right)
-        # keep track of marked, and unmarked nodes in a set w/ coordinates
-        # when no longer 1's end and continue to next node which is unmarked
+        visitedSet = set()
+        numIslands = 0
+
         if not grid:
             return 0
-    
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
-        count = 0
 
-        def bfs(r, c):
-            q = collections.deque()
-            visited.add((r, c))
-            q.append((r, c))
+        rows = len(grid)
+        cols = len(grid[0])
 
-            while q:
-                row, col = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if (r in range(rows)) and ((c in range(cols)) and (grid[r][c] == '1') and ((r, c) not in visited)):
-                        q.append((r, c))
-                        visited.add((r , c))
-
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
-                    bfs(r, c)
-                    count += 1
-
-        return count
-
-            
-
-
-
-
+        # bfs implementation
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1" and (r, c) not in visitedSet:
+                    numIslands += 1
+                    q = deque()
+                    q.append((r, c))
+                    visitedSet.add((r,c))
+                    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                    while q:
+                        row, col = q.popleft()
+                        # check every direction here
+                        for rr, cc in directions:
+                            nr, nc = row + rr, col + cc
+                            if ((0 <= nr < rows and 0 <= nc < cols) and (grid[nr][nc] == '1') and ((nr,nc) not in visitedSet)):
+                                q.append((nr, nc))
+                                visitedSet.add((nr, nc))
+        return numIslands
