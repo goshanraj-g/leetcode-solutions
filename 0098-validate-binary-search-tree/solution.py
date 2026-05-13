55ff4c7ab14b4d87c86554218a2be3267bc08339
Goshanraj Govindaraj <govindag@mcmaster.ca>
2026-05-13 13:26:11 +0000

    [LeetCode] Runtime - 2 ms (76.35%), Memory - 17 MB (56.31%)

--- 0098-validate-binary-search-tree/solution.py
+++ 0098-validate-binary-search-tree/solution.py
@@ -1,18 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def isValid(node, left, right):
+class Solution(object):
+    def isValidBST(self, root):
+        def checkBSTProp(node, low, high):
             if not node:
                 return True
-            if not left < node.val < right:
+
+            if not (low < node.val < high):
                 return False
-            return (isValid(node.left, left, node.val) and
-            isValid(node.right, node.val, right))
-        return isValid(root, float("-inf"), float("inf"))
-        
+
+            return checkBSTProp(node.left, low, node.val) and \
+                    checkBSTProp(node.right, node.val, high)
+
+        return checkBSTProp(root, float("-inf"), float('inf'))
